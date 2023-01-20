7b96f9187866c10e3f0d4212febd23479703dcf5
raskolnikofa <raskolnikofa@icloud.com>
2023-01-20 15:51:54 +0100

    router with routes & pages & components

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route} from "react-router-dom";
+import { HomePage } from "../pages/HomePage";
+import { NotFound } from "../pages/NotFound";
+
+export const AppRouter = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="*" element={<NotFound/>} />
+        </Routes>
+    )
+}
