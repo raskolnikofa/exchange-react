60d4a57062f04a23f009aab0a5f083d8ced1425e
raskolnikofa <raskolnikofa@icloud.com>
2023-01-22 12:01:15 +0100

    sticky search input

--- src/components/HomePage/CurrenciesSearchInput.jsx
+++ src/components/HomePage/CurrenciesSearchInput.jsx
@@ -1,12 +1,29 @@
 import { Grid, Typography, TextField } from '@mui/material';
 import theme from '../../plugins/mui';
+import styles from './CurrenciesSearchInput.module.css';
+
+import { useEffect } from 'react';
+import { useState } from 'react';
 
 export const CurrenciesSearchInput = ({ searchValue, onChange }) => {
+    useEffect(() => {
+        window.addEventListener('scroll', e => handleScroll(e));
+    }, []);
+
+    const [sticky, setSticky] = useState(false);
+
+    const handleScroll = () => {
+        const header = document.querySelector('#header');
+        const headerRect = header.getBoundingClientRect();
+        setSticky(headerRect.top < -50);
+    };
+
     return (
         <Grid
             container
             spacing={2}
             pb={1}
+            className={sticky ? styles.sticky : ''}
             style={{ color: theme.palette.secondary.contrastText, backgroundColor: theme.palette.secondary.main }}
         >
             <Grid
